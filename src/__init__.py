40d4981b2339d970f7e581ada4bbdbf8b97c9e0b
Eli112358 <Eli112358@users.noreply.github.com>
2019-05-12 18:00:32 -0400

    Replace single quotes with double quotes

--- src/__init__.py
+++ src/__init__.py
@@ -4,7 +4,7 @@ from mcpacker.items import *
 import copy
 import json
 
-alphabet_keys = 'abcdefghi'
+alphabet_keys = "abcdefghi"
 
 get_pool = lambda rolls=1, entries=[]: copy.deepcopy({"rolls": rolls, "entries": entries})
 get_entry = lambda type="item", name=resolve("stone"): copy.deepcopy({"type": type, "name": name})
@@ -18,29 +18,29 @@ class DataPacker(DataPack):
         self.tick = Tick(self)
         if auto_process_data:
             self.process_data()
-        print('DataPacker Initialized.')
+        print("DataPacker Initialized.")
     def add_pool(self, path, pool):
         self.copy_loot_table(path).pools.append(pool)
     def copy_loot_table(self, path):
-        my_tables = self['minecraft'].loot_tables
+        my_tables = self["minecraft"].loot_tables
         for name, pack in self.packs.items():
-            tables = pack['minecraft'].loot_tables
+            tables = pack["minecraft"].loot_tables
             if path in tables and not path in my_tables:
                 self.set(resolve(path), copy.deepcopy(tables[path]))
         return my_tables[path]
     def dump(self):
-        print('[dump] Starting...')
+        print("[dump] Starting...")
         def functions(): self.functions.set(self)
         def load(): self.load.set()
-        def tick(): self.tick.set(self.__try_data('objectives'))
+        def tick(): self.tick.set(self.__try_data("objectives"))
         def recipes(): self.recipes()
         try: functions() or load() or tick() or recipes()
         except KeyError: pass
         Built(self).set()
-        super().dump('out', overwrite=True)
-        print('[dump] Complete.')
+        super().dump("out", overwrite=True)
+        print("[dump] Complete.")
     def get_data(self, name):
-        data_file = f'data/{name}.json'
+        data_file = f"data/{name}.json"
         try:
             with open(data_file) as json_data:
                 return json.load(json_data)
@@ -48,73 +48,71 @@ class DataPacker(DataPack):
         except FileNotFoundError as fnf: pass
         # but still catch JSONDecodeError
         except json.decoder.JSONDecodeError as jde:
-            msg,line,col = jde.msg,jde.lineno,jde.colno
-            print(f'(in {data_file}) {msg}: line {line} column {col}')
-            exit()
-    def init_root_advancement(self, icon, description, background = 'stone'):
-        self.set('root', Advancement(
+            exit("(in {}) {}: line {} column {}".format(data_file, jde.msg, jde.lineno, jde.colno))
+    def init_root_advancement(self, icon, description, background = "stone"):
+        self.set("root", Advancement(
             display = {
-                'icon': get_ingredient(self, icon),
-                'title': get_name(self.name),
-                'description': description,
-                'background': resolve(f'textures/blocks/{background}.png')
+                "icon": get_ingredient(self, icon),
+                "title": get_name(self.name),
+                "description": description,
+                "background": resolve(f"textures/blocks/{background}.png")
             },
             criteria = {
-                'never': {
-                    'trigger': resolve('impossible')
+                "never": {
+                    "trigger": resolve("impossible")
                 }
             }
         ))
     def process_data(self):
         self.__load_dependancies()
-        self.functions = Functions(self.__try_data('functions'))
-        self.load = Load(self, self.__try_data('objectives'))
+        self.functions = Functions(self.__try_data("functions"))
+        self.load = Load(self, self.__try_data("objectives"))
     def recipes(self):
-        data = self.__try_data('recipes')
+        data = self.__try_data("recipes")
         if not data:
             return
-        self.set('recipes/root', Advancement(
+        self.set("recipes/root", Advancement(
             display = {
-                'icon': get_ingredient(self, 'piston'),
-                'title': 'Recipe root',
-                'description': 'Recipe root'
+                "icon": get_ingredient(self, "piston"),
+                "title": "Recipe root",
+                "description": "Recipe root"
             },
             criteria = {
-                'impossible': {
-                    'trigger': resolve('impossible')
+                "impossible": {
+                    "trigger": resolve("impossible")
                 }
             }
         ))
-        self.tick.add_text(f'advancement revoke @a from {resolve("recipes/root", self)}\n')
+        self.tick.add_text("advancement revoke @a from {}\n".format(resolve("recipes/root", self)))
         for path, data in data.items():
             shaped = len(data) == 3
             recipe = Recipe(
-                type = 'crafting_shapeless',
-                group = f"{self.tag.suffix(path[:path.index('/')])}",
-                result = {'item': resolve(data[0][1]), 'count': data[0][0]}
+                type = "crafting_shapeless",
+                group = self.tag.suffix(path[:path.index("/")]),
+                result = {"item": resolve(data[0][1]), "count": data[0][0]}
             )
             if shaped:
-                recipe.type = 'crafting_shaped'
+                recipe.type = "crafting_shaped"
                 recipe.pattern = data[2]
                 recipe.key = {}
             else:
                 recipe.ingredients = []
             icon_id = data[0][1]
             advancement = Advancement(
-                parent = resolve('recipes/root', self),
-                rewards = {'recipes': [resolve(path, self)]},
+                parent = resolve("recipes/root", self),
+                rewards = {"recipes": [resolve(path, self)]},
                 display = {
-                    'icon': get_ingredient(self, icon_id),
-                    'title': f'Craftable {get_name(icon_id)}',
-                    'description': f'Craftable {get_name(icon_id)}',
-                    'show_toast': False,
-                    'announce_to_chat': False,
-                    'hidden': True
+                    "icon": get_ingredient(self, icon_id),
+                    "title": f"Craftable {get_name(icon_id)}",
+                    "description": f"Craftable {get_name(icon_id)}",
+                    "show_toast": False,
+                    "announce_to_chat": False,
+                    "hidden": True
                 },
                 criteria = {
-                    'have_items': {
-                        'trigger': resolve('inventory_changed'),
-                        'conditions': {'items': []}
+                    "have_items": {
+                        "trigger": resolve("inventory_changed"),
+                        "conditions": {"items": []}
                     }
                 }
             )
@@ -124,34 +122,31 @@ class DataPacker(DataPack):
                     recipe.key[alphabet_keys[i]] = entry
                 else:
                     recipe.ingredients.append(entry)
-                advancement.criteria['have_items']['conditions']['items'].append(entry)
+                advancement.criteria["have_items"]["conditions"]["items"].append(entry)
             self.set(path, recipe)
-            if 'recipe_advancement' in self.data:
-                if self.data['recipe_advancement']:
-                    self.set(f'recipes/{path}', advancement)
+            if "recipe_advancement" in self.data:
+                if self.data["recipe_advancement"]:
+                    self.set(f"recipes/{path}", advancement)
     def set(self, path, value, vanilla = False):
-        this_path = resolve(path, self)
-        if ':' in path:
-            this_path = path
-        self[this_path] = value
+        self[path if ":" in path else resolve(path, self)] = value
     def __load_dependancies(self):
-        data = self.__try_data('dependancies')
+        data = self.__try_data("dependancies")
         if not data:
             return
         self.packs = {}
         for name in data:
-            print(f'[dependancies] Loading \'{name}\'...')
-            self.packs[name] = DataPack.load(f'out/{name}')
-        print(f'[dependancies] Complete.')
+            print("[dependancies] Loading '{}'...".format(name))
+            self.packs[name] = DataPack.load("out/" + name)
+        print("[dependancies] Complete.")
     def __try_data(self, name):
         try: return self.data[name]
         except KeyError: return []
         except TypeError: return []
 
-class GlobalName(object):
+class GlobalName(str):
     def __init__(self, name):
         self.name = name
     def __str__(self):
         return self.name
     def suffix(self, suffix):
-        return GlobalName(f'{self.name}_{suffix}')
+        return GlobalName("_".join([self.name, suffix]))
