de645d74abb91abcac58bd804d2bb763e0284f16
Eli112358 <Eli112358@users.noreply.github.com>
2019-05-12 23:04:42 -0400

    Implement list comprehension Replace single quotes with double quotes

--- src/functions.py
+++ src/functions.py
@@ -4,58 +4,48 @@ from datetime import datetime
 import json
 
 colors = [
-    'black',
-    'dark_blue',
-    'dark_green',
-    'dark_aqua',
-    'dark_red',
-    'dark_purple',
-    'gold',
-    'gray',
-    'dark_gray',
-    'blue',
-    'green',
-    'aqua',
-    'red',
-    'light_purple',
-    'yellow'
+    "black",
+    "dark_blue",
+    "dark_green",
+    "dark_aqua",
+    "dark_red",
+    "dark_purple",
+    "gold",
+    "gray",
+    "dark_gray",
+    "blue",
+    "green",
+    "aqua",
+    "red",
+    "light_purple",
+    "yellow"
 ]
 
-join = lambda lines: '\n'.join(lines)
-
 class FunctionWrapper(object):
-    def __init__(self, body = ''):
+    def __init__(self, body=""):
         self.function = Function(body)
     def add_text(self, text):
         self.function.body += text
     def add_lines(self, lines):
-        self.add_text(join(lines) + ("\n" if len(lines) else ""))
+        self.add_text("\n".join(lines) + ("\n" if len(lines) else ""))
     def add_indexed(self, size, text):
-        self.add_lines([text.replace("{i}", f"{i}") for i in range(size)])
-    def set(self, pack, path, callback = None):
+        self.add_lines([text.format(i) for i in range(size)])
+    def set(self, pack, path, callback=None):
         if not len(self.function.body): return
         pack[path] = self.function
         if callback: callback()
 
 class Functions(dict):
     def __init__(self, relpaths):
-        for relpath in relpaths:
-            self.add(relpath)
-    def add(self, relpath, body = ['']):
-        self[relpath] = FunctionWrapper(join(body))
-    def add_data(self, templates, data, indexed = False):
-        def get_data_str(name, data0):
-            str = templates[name][0:]
-            for key, value in data0.items():
-                str = str.replace(key, value)
-            return str
-        for name, list in data.items():
-            for entry in list:
-                data_str = get_data_str(name, entry['data'])
-                if indexed:
-                    self[name].add_indexed(entry['n'], data_str)
-                else:
-                    self[name].add_text(data_str)
+        [self.add(relpath) for relpath in relpaths]
+    def add(self, relpath, body=[""]):
+        self[relpath] = FunctionWrapper("\n".join(body))
+    def add_data(self, pack):
+        data_lines = lambda path, object, entry, i: pack.data["function_templates"][path]["template"].format(*data_format(object, entry, i))
+        data_format = lambda object, entry, i: [i]+entry[1:] if "iterator" in object else entry
+        data_range = lambda object, entry: entry[0] if "iterator" in object else 1
+        data_lines1 = lambda object, path, entry: [data_lines(path, object, entry, i) for i in range(data_range(object, entry))]
+        [self[path].add_lines(data_lines1(object, path, entry)) for path,object in pack.data["function_templates"].items() for entry in object["data"]]
     def load(self, pack):
         if not "function_code" in pack.data: return
         [self[path].add_lines(lines) for path,lines in pack.data["function_code"].items()]
@@ -64,7 +54,7 @@ class Functions(dict):
             func.set(pack, resolve(path, pack))
 
 class SelfTaggedFunction(FunctionWrapper):
-    def __init__(self, pack, relpath, body = '', namespace = 'minecraft'):
+    def __init__(self, pack, relpath, body="", namespace="minecraft"):
         self.pack = pack
         self.relpath = relpath
         self.namespace = namespace
@@ -80,8 +70,8 @@ class SelfTaggedFunction(FunctionWrapper):
         super().set(self.pack, self.fullpath, lambda: self.add_to_tag(self.fullpath))
 
 class Load(SelfTaggedFunction):
-    def __init__(self, pack, objectives = []):
-        super().__init__(pack, 'load')
+    def __init__(self, pack, objectives=[]):
+        super().__init__(pack, "load")
         self.add_lines([f"scoreboard objectives add {name}" for name in objectives])
 
 class Tick(SelfTaggedFunction):
@@ -90,8 +80,8 @@ class Tick(SelfTaggedFunction):
         "add @a {} 0",
         "enable @a {}"
     ]
-    def __init__(self, pack, body = ''):
-        super().__init__(pack, 'tick', body)
+    def __init__(self, pack, body = ""):
+        super().__init__(pack, "tick", body)
         if pack.data and "options" in pack.data:
             self.add_lines([pack.data["options"]["pattern"].format(*values) for values in pack.data["options"]["features"]])
     def set(self, objectives = []):
@@ -102,16 +92,14 @@ class Tick(SelfTaggedFunction):
 class Built(SelfTaggedFunction):
     def __init__(self, pack):
         today = datetime.today()
-        date_str = today.strftime('%Y-%m-%d')
-        day_of_year = today.timetuple().tm_yday
-        message = json.dumps(['',
+        tellraw = "tellraw @s " + json.dumps(["",
             {
-                'text': date_str,
-                'color': colors[day_of_year%len(colors)]
+                "text": today.strftime("%Y-%m-%d"),
+                "color": colors[today.timetuple().tm_yday%len(colors)]
             },
             {
-                'text': f' | {pack.name}',
-                'color': 'white'
+                "text": f" | {pack.name}",
+                "color": "white"
             }
         ])
-        super().__init__(pack, 'built', f'tellraw @s {message}', 'main')
+        super().__init__(pack, "built", tellraw, "main")
