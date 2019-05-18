96a3c69b9ede4ddd93dafbc38b98aaced5b1733c
Eli112358 <Eli112358@users.noreply.github.com>
2019-05-18 17:09:35 -0400

    Refactor villager Use dataclass

--- src/villager.py
+++ src/villager.py
@@ -1,58 +1,62 @@
 from mcpack import (Function, FunctionTag)
-from mcpacker.items import (escape, get_name, resolve)
+from typing import Optional
+from dataclasses import (dataclass, field)
 import copy
 
-class Trade(object):
-    def __init__(self, buy, sell, buyB = None, rewardExp = 1, maxUses = 1000, uses = 0, xp = 0, priceMultiplier = 1.0, specialPrice = 0, demand = 0):
-        self.buy = buy
-        self.buyB = buyB
-        self.sell = sell
-        self.rewardExp = rewardExp
-        self.maxUses = maxUses
-        self.uses = uses
-        self.xp = xp
-        self.priceMultiplier = priceMultiplier
-        self.specialPrice = specialPrice
-        self.demand = demand
-    def dump(self):
-        result = f'{{buy:{self.buy.trade()},'
-        if self.buyB:
-            result += f'buyB:{self.buyB.trade()},'
-        result += f'sell:{self.sell.trade()},'
-        result += f'rewardExp:{self.rewardExp}b,'
-        result += f'maxUses:{self.maxUses},'
-        result += f'uses:{self.uses},'
-        result += f'xp:{self.xp},'
-        result += f'priceMultiplier:{self.priceMultiplier},'
-        result += f'specialPrice:{self.specialPrice},'
-        result += f'demand:{self.demand}'
-        result += '}'
-        return result
+from .items import *
 
+@dataclass
+class Trade(object):
+    template = '{{buy:{},{}sell:{},rewardExp:{},maxUses:{},uses:{},xp:{},priceMultiplier:{},specialPrice:{},demand:{}}}'
+
+    buy: Item
+    sell: Item
+    buyB: Optional[Item] = None
+    rewardExp: Optional[int] = 1
+    maxUses: Optional[int] = 1000
+    uses: Optional[int] = 0
+    xp: Optional[int] = 0
+    priceMultiplier: Optional[float] = 1.0
+    specialPrice: Optional[int] = 0
+    demand: Optional[int] = 0
+    def dump(self):
+        values = [
+            self.buy.trade(),
+            'buyB:{},'.format(self.buyB.trade()) if self.buyB else '',
+            self.sell.trade(),
+            self.rewardExp,
+            self.maxUses,
+            self.uses,
+            self.xp,
+            self.priceMultiplier,
+            self.specialPrice,
+            self.demand
+        ]
+        return self.template.format(*values)
+
+@dataclass
 class Villager(object):
-    def __init__(self, name, coords, profession, biome = "plains", trades = []):
-        self.name = name
-        self.escaped_name = escape(f'"{get_name(name)}"')
-        self.coords = coords
-        self.profession = profession
-        self.biome = biome
-        self.trades = copy.deepcopy(trades)
+    template = 'summon villager {} {{CustomName:{},VillagerData:{{level:8,profession:{},type:{}}},Tags:[{}],{},DeathLootTable:{},CanPickUpLoot:0,Offers:{{Recipes:[{}]}}}}'
+
+    name: str
+    coords: str
+    profession: str
+    biome: Optional[str] = 'plains'
+    trades: Optional[list] = field(default_factory=list)
     def set(self, pack, root_name):
-        trade_dump = []
-        for trade in self.trades:
-            trade_dump.append(trade.dump())
-        body = f'summon villager {self.coords} {{'
-        body += f'CustomName:"{self.escaped_name}",'
-        body += 'VillagerData:{level:8,'
-        body += f'profession:"{resolve(self.profession)}",'
-        body += f'type:"{resolve(self.biome)}"'
-        body += '},Tags:["shop","villager"],'
-        body += 'Invulnerable:1,NoGravity:1,Silent:1,'
-        body += 'NoAI:1,DeathLootTable:"empty",CanPickUpLoot:0,'
-        body += 'Offers:{Recipes:['
-        body += ','.join(trade_dump)
-        body += ']}}\n'
-        pack[f'{pack.name}:villagers/{self.name}'] = Function(body)
+        get_list = lambda func,values: ','.join([func(value) for value in values])
+        values = [
+            self.coords,
+            quote(escape(quote(self.name))),
+            quote(resolve(self.profession)),
+            quote(resolve(self.biome)),
+            get_list(lambda v: quote(v), ['shop', 'villager']),
+            get_list(lambda v: v+':1', ['Invulnerable', 'NoGravity', 'Silent', 'NoAI']),
+            quote(resolve('empty')),
+            get_list(lambda v: v.dump(), self.trades)
+        ]
+        path = 'villagers/'+self.name
+        pack.set(path, Function(self.template.format(*values)))
         if not 'villagers' in pack[root_name].function_tags:
-            pack[resolve('villagers', None, root_name)] = FunctionTag([])
-        pack[root_name].function_tags['villagers'].values.append(resolve(f'villagers/{self.name}', pack))
+            pack.set(resolve('villagers', None, root_name), FunctionTag())
+        pack[root_name].function_tags['villagers'].values.append(resolve(path, pack))
