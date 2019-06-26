a0b5ad594582b5044783c9a11f90e01309c219d2
Eli112358 <Eli112358@users.noreply.github.com>
2019-06-26 08:36:12 -0400

    Fix typo

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='mcpacker',
     version='0.1',
-    description="Wrapper for vberlie's mcpack",
+    description="Wrapper for vberlier's mcpack",
     author='Eli112358',
     packages=['mcpacker'],
     package_dir={'mcpacker': 'src'},
